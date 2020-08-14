32e9689e973d9a3044801ecb39dce141ced7aa30
Nandana31 <68599451+Nandana31@users.noreply.github.com>
2020-08-14 20:13:19 +0530

    Js

---
+++ chat.js
@@ -0,0 +1,46 @@
+let name = prompt("Hloo,Whats Your Name")
+alert(`Hello ${name} , I Am A Surprise ChatBot Made By Nandana`)
+let botName = prompt("I Have No Name , Can You Please  Give Me A Name");
+alert(`${botName} Is A Good Name For Me 😅`)
+let age = prompt(`Hey ${name}, What's your biggest dream`)
+alert(`Oh Good! that's great!`)
+let con = prompt("In Which Country You Live?")
+if (con.includes("india")){
+  alert("Nandana Is Also From India ")
+}else if (con.includes("India")){
+  alert("Nandana Is Also From India ")
+}
+else {
+  alert(`${con} Is A Good Country `)
+}
+var lang = prompt("Which Is Your Favourite Programming Language?")
+let lang1 = lang.toUpperCase()
+
+  if (lang1.includes("YTHON")){
+    alert("Python Is A Good Language 😅\n__M r . R c__ Is Expert In Python 👨‍💻")
+}
+else if (lang1.includes("AVA")){
+    alert("Android Developer Spotted 🔫\nHands Up 🙃")
+}
+else if (lang1.includes("SCRIPT")){
+    alert("Super Human Spotted 🤯\nHands Up  ")
+}
+else if (lang1.includes("SSEMBLY")){
+    alert("You Are A Hard Worker Bro 😓\n")
+}
+else if (lang1.includes("TML")){
+    alert("Nasa Hacker Spotted  😶🔫\nHands Up 🙃")
+}
+else if (lang1.includes("C")){
+    alert("You Are Fater Than Python 🐍")
+}
+else if (lang.includes("++")){
+    alert("Flash Spotted%%")
+}
+else if (lang.includes("ASH")){
+    alert("Hacker Spotted 😱")
+}
+else {
+  alert(`Smartest Person On The Earth Spotted`)
+}
+alert("Thanks For Using My Bot")
